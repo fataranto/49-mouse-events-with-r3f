895e2fa5bd751a10b3449fcd04bd54983e6875c5
Fabian <fabian@Big-Mac-de-Fabian.local>
2023-11-03 18:49:54 +0100

    stopPropagation

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -11,21 +11,30 @@ export default function Experience()
         cube.current.rotation.y += delta * 0.2
     })
 
+    const eventHandler = () =>
+    {
+        cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`)
+    }
+
+   
+
     return <>
+
+        
 
         <OrbitControls makeDefault />
 
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        <mesh position-x={ - 2 }>
+        <mesh position-x={ - 2 } onClick={ (e) => e.stopPropagation() }>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
+        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 } onClick={ eventHandler }>
             <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
+            <meshStandardMaterial  />
         </mesh>
 
         <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
